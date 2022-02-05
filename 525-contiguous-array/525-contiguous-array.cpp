fa40767af53e88cd32fd8fffedc0c060750505f4
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-02-05 19:28:26 +0530

    Time: 189 ms (43.03%), Space: 83.8 MB (86.89%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        vector<int> n0; 
-        vector<int> n1; 
-        vector<int> diffv; 
-        unordered_map<int, int> mp; 
-        int ans = 0;
-        int num0 = 0, num1 = 0;
-        for(int i = 0 ; i < nums.size() ; i++) {
-            if(nums[i] == 0)
-                num0++;
-            if(nums[i] == 1)
-                num1++;
-            n0.push_back(num0);
-            n1.push_back(num1);
-            int diff = n0[i] - n1[i];
-            diffv.push_back(diff);
-            if(mp.find(diff) == mp.end())
-                mp[diff] = i;
-            else
-                ans = max(ans, i - mp[diff]);
-            if(diff == 0)
-                ans = max(ans, i+1);
+        int sum=0;
+        int n=nums.size();
+        int longestSubArray=0;
+        unordered_map<int,int> um;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) sum+=-1;
+            else sum+=1;
+            if(sum==0) longestSubArray=max(longestSubArray,i+1);
+            if(um.find(sum)!=um.end()){
+                longestSubArray=max(longestSubArray,i-um[sum]);
+            }else{
+                um[sum]=i;
+            }
         }
-        return ans;
+        return longestSubArray;
     }
 };
