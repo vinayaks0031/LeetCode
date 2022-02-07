5312987cf70d51f372f53504eb4f1911b9d9ff22
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-02-07 23:30:33 +0530

    Time: 3 ms (78.44%), Space: 6.5 MB (98.61%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char ans=0;
+        int sSum=0;
+        int tSum=0;
         
-        vector<int> sHash(26,0);
-        vector<int> tHash(26,0);
-        int nt=t.size();
-        int ns=s.size();
-        for(int i=0;i<nt;i++){
-            tHash[t[i]-'a']++;
-        }
-        for(int i=0;i<ns;i++){
-            sHash[s[i]-'a']++;
-        }
-        for(int i=0;i<26;i++){
-            if(sHash[i]!=tHash[i]){
-                ans='a'+i;
-                break;
-            }
-        }
-        return ans;
+        for(auto i:s) sSum+=i;
+        for(auto i:t) tSum+=i;
+        
+        return char(tSum-sSum);
     }
 };
