e723793bc846244412e1c52351a41791d08ff88a
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-02-19 14:06:44 +0530

    Time: 17 ms (20.18%), Space: 13.7 MB (65.05%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
-    int lastOcc(vector<int>& nums, int target, int n)
+
+    int occ(vector<int>& nums, int target, int n)
     {
         int low=0,high=n-1;
-        int ans=-1;
+        
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target){
+            if(nums[mid]<target){
                 low=mid+1;
             }else{
                 high=mid-1;
-            }
-            
+            }    
         }
-        return ans;
-    }
-    
-    int firstOcc(vector<int>& nums, int target, int n)
-    {
-        int low=0,high=n-1;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }else{
-                high=mid-1;
-            }
-            
-        }
-        return ans;
+        return low;
     }
     
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
+
         int n=nums.size();
-        ans.push_back(firstOcc(nums,target,n));        
-        ans.push_back(lastOcc(nums,target,n));
-        return ans;
+        int first=occ(nums,target,n);        
+        int last=occ(nums,target+1,n)-1;
+        if( first<n && nums[first]==target) return {first,last};
+        return {-1,-1};
     }
 };
