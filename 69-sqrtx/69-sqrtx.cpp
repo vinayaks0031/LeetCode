b099c8e86bb44cdcfddf5176219236b95dc392d6
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-02-20 20:26:19 +0530

    Time: 4 ms (56.40%), Space: 6 MB (26.26%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long start=1,end=x;
+        long ans=0;
+        
+        while(start<=end)
+        {
+            long long mid=start+(end-start)/2;
+            if((mid*mid)==x) return mid;
+            else if((mid*mid)<x)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;    
+            }
+        }
+        return ans;
+    }
+};
