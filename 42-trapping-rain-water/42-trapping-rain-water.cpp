2e9ef165b641f533a6cc4827f0d9a870491ca358
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-02-25 11:30:34 +0530

    Time: 24 ms (17.50%), Space: 17.8 MB (14.53%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -7,7 +7,8 @@ public:
         
         //bruteforce approach o(n^2)
         
-//         for(int i=0;i<n;i++){
+//         for(int i=0;i<n;i++)
+//         {
 //             int leftmax=0;
 //             int rightmax=0;
 //             //leftmax
@@ -20,19 +21,56 @@ public:
         
         //1st optimization (array of prefix ans postfix max) time:o(n) space:o(2n)
         
-        vector<int> prefixMax(n,0),postfixMax(n,0);
+//         vector<int> prefixMax(n,0),postfixMax(n,0);
+//         //prefix max
+//         for(int i=1;i<n;i++) prefixMax[i]=max(prefixMax[i-1],height[i-1]);
+//         //postfix max 
+//         for(int i=n-2;i>=0;i--) postfixMax[i]=max(postfixMax[i+1],height[i+1]);
         
-        //prefix max
-        for(int i=1;i<n;i++) prefixMax[i]=max(prefixMax[i-1],height[i-1]);
-        
-        //postfix max 
-        for(int i=n-2;i>=0;i--) postfixMax[i]=max(postfixMax[i+1],height[i+1]);
-        
-        // calculating sum
-        for(int i=0;i<n;i++) ans+=max(0,(min(prefixMax[i],postfixMax[i])-height[i]));
+//         for(int i=0;i<n;i++) ans+=max(0,(min(prefixMax[i],postfixMax[i])-height[i]));
         
         
-        //2nd optimization
+        //2nd optimization (only array of postfix max) time:o(n) space:o(n)
+        
+//         int prefixMax=0;
+//         vector<int> postfixMax(n,0);
+//         //postfix Max
+//         for(int i=n-2;i>=0;i--) postfixMax[i]=max(postfixMax[i+1],height[i+1]);
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             ans+=max(0,(min(prefixMax,postfixMax[i])-height[i]));
+//             prefixMax=max(prefixMax,height[i]);
+//         }
+        
+//last the most optimized Solution time:o(n) space:o(1)
+        
+        int maxIdx=-1;
+        int maxValue=-1;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(maxValue<height[i])
+            {
+                maxValue=height[i];
+                maxIdx=i;
+            }
+        }
+        
+        //check for values before maxIdx
+        int currMax=0;
+        for(int i=0;i<maxIdx;i++)
+        {
+            ans+=max(0,min(currMax,maxValue)-height[i]);
+            currMax=max(currMax,height[i]);
+        }
+        //check for values after maxIdx
+        currMax=0;
+        for(int i=n-1;i>maxIdx;i--)
+        {
+            ans+=max(0,min(currMax,maxValue)-height[i]);
+            currMax=max(currMax,height[i]);
+        }
         return ans;
     }
 };
