8bcc471d6fd21aa3baf9c638eb03e654566afe0a
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-02-28 16:51:55 +0530

    Time: 129 ms (66.67%), Space: 28.9 MB (77.78%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         
-        int sn=s.size(),tn=t.size();
         vector<int> shash(26,0);
-        vector<int> thash(26,0);
-        
-        for(auto &i:s) shash[i-'a']++;
-        for(auto &i:t) thash[i-'a']++;
-        
         int ans=0;
+        for(auto &i:s) shash[i-'a']++;
+        for(auto &i:t) shash[i-'a']--;
         
-        for(int i=0;i<26;i++)
-        {
-            if(shash[i]>0 && thash[i]>0) ans+= shash[i]>thash[i] ? thash[i] : shash[i]; 
-        }
+        for(int i=0;i<26;i++) ans+=abs(shash[i]); 
         
-        return (sn+tn)-(ans*2);
+        return ans;
 
     }
 };
