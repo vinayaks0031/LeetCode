b32a589da54b56aeb369fce12851542ad8206307
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-03-01 23:35:17 +0530

    Time: 5 ms (68.32%), Space: 8.6 MB (21.58%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> ans;
+        
+        for (int i=0;i<=num;i++)
+        {
+            ans.push_back(__builtin_popcount(i));
+        }
+
+        return ans;
+    }
+};
