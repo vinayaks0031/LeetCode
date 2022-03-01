aa1437580d1b03ed4cc7d9065d7cebae196c0883
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-03-01 15:03:46 +0530

    Time: 21 ms (16.79%), Space: 5.9 MB (80.03%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fibo(int n){
+        if( n<=1 ) return n;
+        return fibo(n-1)+fibo(n-2);
+    }
+    int fib(int n) { 
+        
+        return fibo(n);
+
+    }
+};
