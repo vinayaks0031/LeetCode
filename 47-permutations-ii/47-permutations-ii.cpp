78ce2bd3ca8ef53338d1bf5c4ccd4ffd32398cf2
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-03-15 14:16:22 +0530

    Time: 18 ms (44.52%), Space: 9.2 MB (64.74%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -7,16 +7,21 @@ private:
             return;
         }
         
+        int prev=-11;
         for(int i=idx;i<n;i++)
         {
-            ds.push_back(nums[i]);
-            int tmp=nums[idx];
-            nums[idx]=nums[i];
-            nums[i]=tmp;
-            makeUniquePermutations(idx+1,nums,ds,tmpAns,n);
-            nums[i]=nums[idx];
-            nums[idx]=tmp;
-            ds.pop_back();
+            if(prev!=nums[i])
+            {
+                prev=nums[i];
+                ds.push_back(nums[i]);
+                int tmp=nums[idx];
+                nums[idx]=nums[i];
+                nums[i]=tmp;
+                makeUniquePermutations(idx+1,nums,ds,tmpAns,n);
+                nums[i]=nums[idx];
+                nums[idx]=tmp;
+                ds.pop_back();
+            }
         }
     }
 public:
@@ -25,6 +30,7 @@ public:
         set<vector<int>> tmpAns;
         vector<int> ds;
         int n=nums.size();
+        sort(nums.begin(),nums.end());
         makeUniquePermutations(0,nums,ds,tmpAns,n);
         vector<vector<int>> ans(tmpAns.begin(),tmpAns.end());
         return ans;
