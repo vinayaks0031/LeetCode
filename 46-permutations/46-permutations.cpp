26b90049c48cf69a8f8c67d3ab65a00453b5d576
Vinayak Sharma <76878117+vinayaks0031@users.noreply.github.com>
2022-03-15 12:59:29 +0530

    Time: 11 ms (13.68%), Space: 8.7 MB (10.43%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,21 +1,21 @@
 class Solution {
 private:
-    void makeAllPermutations(int idx,vector<int> nums,vector<int> ds,unordered_map<int,int> umap, vector<vector<int>>& ans,int n)
+    void makeAllPermutations(int idx,vector<int> nums,vector<int> ds,vector<vector<int>>& ans,int n)
     {
-        if(idx==n){
-           ans.push_back(ds);  
+        if(idx==n)
+        {
+            ans.push_back(ds);
+            return ;
         }
         
-        for(int i=0;i<n;i++)
+        for(int i=idx;i<n;i++)
         {
-            if(umap[nums[i]]==1)
-            {
-                umap[nums[i]]--;
-                ds.push_back(nums[i]);
-                makeAllPermutations(idx+1,nums,ds,umap,ans,n);
-                ds.pop_back();
-                umap[nums[i]]++;
-            }
+            ds.push_back(nums[i]);
+            int temp=nums[idx];
+            nums[idx]=nums[i];
+            nums[i]=temp;
+            makeAllPermutations(idx+1,nums,ds,ans,n);
+            ds.pop_back();
         }
     }
 public:
@@ -23,13 +23,8 @@ public:
     {
         vector<vector<int>> ans;
         vector<int> ds;
-        unordered_map<int,int> umap;
-        for(auto &i:nums)
-        {
-            umap[i]++;
-        }
         int n=nums.size();
-        makeAllPermutations(0,nums,ds,umap,ans,n);
+        makeAllPermutations(0,nums,ds,ans,n);
         return ans;
     }
 };
